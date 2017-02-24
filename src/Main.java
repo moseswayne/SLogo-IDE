dfcc415c309ebf06c4d4c448d478ba1a60fc87fa
Yuxiang He <yuxiang.he@duke.edu>
2017-02-23 21:36:12 -0500

    more stuff

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

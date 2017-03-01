c2741e2d8462c26a438df879934db3666a055b19
KElbert <ke60@duke.edu>
2017-02-28 19:03:42 -0500

    Added turtle model

---
+++ src/Operations/TurtleModel.java
@@ -0,0 +1,62 @@
+package Operations;
+
+class TurtleModel {
+	private double myX;
+	private double myY;
+	private double myHeading;
+	private boolean isPenShowing;
+	private boolean isTurtleShowing;
+
+	protected TurtleModel() {
+		myX = 0;
+		myY = 0;
+		myHeading = 0;
+		isPenShowing = true;
+		isTurtleShowing = true;
+	}
+
+	protected double getX() {
+		return myX;
+	}
+
+	protected double getY() {
+		return myY;
+	}
+
+	protected double getHeading() {
+		return myHeading;
+	}
+
+	protected double setX(double newX) {
+		return myX = newX;
+	}
+
+	protected double setY(double newY) {
+		return myY = newY;
+	}
+
+	protected double setHeading(double newHeading) {
+		return myHeading = newHeading;
+	}
+
+	protected boolean isPenShowing() {
+		return isPenShowing;
+	}
+
+	protected boolean isTurtleShowing() {
+		return isTurtleShowing;
+	}
+
+	protected boolean isTurtleShowing(boolean visibility) {
+		return isTurtleShowing = visibility;
+	}
+
+	protected boolean isPenShowing(boolean visibility) {
+		return isPenShowing = visibility;
+	}
+
+	protected double distance(double x, double y) {
+		return Math.sqrt((x - this.myX) * (x - this.myX) + (y - this.myY) * (y - this.myY));
+	}
+
+}

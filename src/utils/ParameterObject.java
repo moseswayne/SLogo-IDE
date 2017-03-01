5f6884efcc66942bafae20e5f6379d34cdb892f2
Moses Wayne <msw38@duke.edu>
2017-03-01 00:49:54 -0500

    Updated utility package and view data to more consistently manage information passed from the backend

--- src/utils/ParameterObject.java
+++ src/utils/ParameterObject.java
@@ -1,6 +1,43 @@
 package utils;
 
+import java.util.List;
+import java.util.Map;
+
 public class ParameterObject {
 
-	public ParameterObject(List)
+	private List<String> parameterList;
+	private Map<String, Double> varMap;
+	private TurtleModel myTurtle;
+	
+	public ParameterObject(List<String> params, Map<String, Double> vars, TurtleModel turt) {
+		this(params, vars);
+		myTurtle = turt;
+	}
+	
+	public ParameterObject(List<String> params, Map<String, Double> vars) {
+		parameterList = params;
+		varMap = vars;
+	}
+	
+	public Double getElement(int index) {
+		try {
+			String val = parameterList.get(index);
+			if(varMap.containsKey(val)) {
+				return varMap.get(val);
+			} else {
+				try {
+					Double.parseDouble(val);
+				} catch (Exception NumberFormatException){
+					
+				}
+			}
+		} catch (Exception NullPointerException){
+			//do something
+		}
+		return null;
+	}
+	
+	public TurtleModel getTurtle() {
+		return myTurtle;
+	}
 }

8586173106333aa740a14ebbedd99ddee6c0c58d
KElbert <ke60@duke.edu>
2017-02-28 20:19:50 -0500

    Modified execute command

--- src/Operations/CommandOperation.java
+++ src/Operations/CommandOperation.java
@@ -1,8 +1,10 @@
 package Operations;
 
 import View.FrontEndData;
+import utils.TurtleModel;
 
 public interface CommandOperation {
+	public ParameterObject execute(ParameterObject params);
 
 	public FrontEndData execute();
 }

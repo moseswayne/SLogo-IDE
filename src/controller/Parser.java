85f99b3f7c03f3cb1bf1b24ad6925b1bb10d53b0
jyang301 <jy183@duke.edu>
2017-03-01 19:20:11 -0500

    basic parser implementation

---
+++ src/controller/Parser.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Parser {
+	
+	public ArrayList<String> parseCommand(String cmd) {
+		
+	}
+	
+	private String getRootCommand(String cmd) {
+		return cmd.split(" ")[0];
+	}
+}

0731d05357b2da5ffd98a58dcad3e5cbf4e60708
jyang301 <jy183@duke.edu>
2017-03-02 01:19:23 -0500

    begin node class, modify parser

--- src/controller/Parser.java
+++ src/controller/Parser.java
@@ -1,30 +1,37 @@
 package controller;
 
 import utils.ParameterObject;
+
 import java.util.Map;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.File;
+import java.util.ResourceBundle;
 import java.util.ArrayList;
 
 public class Parser {
 	
-	private String cmd;
-	private Map<String, String> cmdMap;
-	private Map<String, Double> varMap;
+	private ResourceBundle myResourceBundle;
+	private String command;
+	private Map<String, String> commandsMap;
+	private Map<String, Double> variablesMap;
 	private ParameterObject params;
 	
-	public Parser(String command, Map<String, String> commandMap, Map<String, Double> variableMap) {
-		cmd = command;
-		cmdMap = commandMap;
-		varMap = variableMap;
-		parseCommand();
-	}
-	
-	public ParameterObject getParameters {
+	public Parser(Map<String, Double> varMap) {
+		variablesMap = varMap;
+		myResourceBundle = ResourceBundle.getBundle("resources.languages/Syntax.properties");
+		
 		
 	}
 	
-	private void parseCommand() {
+	public ParameterObject getParameters() {
+		return params;
+	}
+	
+	private ArrayList<Node> parseCommand(String cmd) {
+		command = cmd;
 		String rootCmd = getRootCommand(cmd);
-		
 	}
 	
 	private String getRootCommand(String cmd) {

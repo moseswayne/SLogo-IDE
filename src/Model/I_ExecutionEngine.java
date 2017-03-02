b67244260d4f0309b30d841e97bb13dd323b39e5
Moses Wayne <msw38@duke.edu>
2017-03-01 21:53:12 -0500

    Fixed various operation calls and back end control of calls

--- src/Model/I_ExecutionEngine.java
+++ src/Model/I_ExecutionEngine.java
@@ -3,9 +3,10 @@ package Model;
 import java.util.Collection;
 
 import View.FrontEndData;
+import utils.RawCommand;
 
 public interface I_ExecutionEngine {
 
-	public Collection<FrontEndData> runOp(RawCommand runCommand);
+	public FrontEndData runOp(RawCommand runCommand);
 	
 }

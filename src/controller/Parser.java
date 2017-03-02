11e586b3cbb62ca272e0f8f2a25d9132cb4981c5
jyang301 <jy183@duke.edu>
2017-03-02 02:26:04 -0500

    parser and node updates

--- src/controller/Parser.java
+++ src/controller/Parser.java
@@ -3,34 +3,43 @@ package controller;
 import utils.ParameterObject;
 
 import java.util.Map;
-import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.File;
 import java.util.ResourceBundle;
 import java.util.ArrayList;
-import java.io.FileInputStream;
-import java.io.File;
-
+import java.util.regex.Pattern;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Enumeration;
+import java.util.AbstractMap.SimpleEntry;
 
 public class Parser {
 
-	private ResourceBundle myResourceBundle;
 	private String command;
 	private Map<String, String> commandsMap;
 	private Map<String, Double> variablesMap;
 	private ParameterObject params;
+	private List<Entry<String, Pattern>> mySymbols;
 	
 	public Parser(Map<String, Double> varMap) {
 		variablesMap = varMap;
-		myResourceBundle = ResourceBundle.getBundle("resources.languages/Syntax.properties");
+		mySymbols = new ArrayList<>();
 	}
 	
 	public ParameterObject getParameters() {
 		return params;
 	}
 	
-	private ArrayList<Node> parseCommand(String cmd) {
+	public void addPatterns (String syntax) {
+        ResourceBundle resources = ResourceBundle.getBundle(syntax);
+        Enumeration<String> iter = resources.getKeys();
+        while (iter.hasMoreElements()) {
+            String key = iter.nextElement();
+            String regex = resources.getString(key);
+            mySymbols.add(new SimpleEntry<>(key,
+                           Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
+        }
+    }
+	
+	private ArrayList<A_Node> parseCommand(String cmd) {
 		command = cmd;
 		String rootCmd = getRootCommand(cmd);
 	}
@@ -40,6 +49,20 @@ public class Parser {
 		if (!isValidCommand(rootCmd)) throw new ParserException(ParserException.INVALID_CMD);
 		return rootCmd;
 	}
+	
+	public String getSymbol (String text) {
+        final String ERROR = "NO MATCH";
+        for (Entry<String, Pattern> e : mySymbols) {
+            if (match(text, e.getValue())) {
+                return e.getKey();
+            }
+        }
+        return ERROR;
+    }
+	
+	private boolean match (String text, Pattern regex) {
+        return regex.matcher(text).matches();
+    }
 	
 	private boolean isValidCommand(String cmd) {
 		return commandsMap.containsKey(cmd) && commandsMap.get(cmd).length() != 0;

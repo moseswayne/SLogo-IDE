e40c9aaa6e1197a36c946e537763f4c3efef95c8
jyang301 <jy183@duke.edu>
2017-03-09 01:48:34 -0500

    parser works for everything but control operations

--- src/controller/Parser.java
+++ src/controller/Parser.java
@@ -9,8 +9,6 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Collection;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
 import java.util.regex.Pattern;
 import java.lang.StringBuilder;
 
@@ -44,10 +42,18 @@ public class Parser {
 		syntaxProperties = getPropertiesFile(SYNTAX_PROPERTIES_FILE);
 	}
 	
-	private Collection<CommandNode> parseCommand(String expression) {
+	public Collection<CommandNode> parseCommand(String expression) {
+		ArrayList<CommandNode> cmdTrees = new ArrayList<CommandNode>(0);
 		expression = removeCommentsAndFlatten(expression);
 		Tokenizer tokens = new Tokenizer(expression, WHITESPACE);
-		
+		while (tokens.hasNextToken()) {
+			String rootCmd = tokens.nextToken();
+			if (!operationFactory.isValidOp(rootCmd)) {
+				throw new ParserException(ParserException.INVALID_CMD, rootCmd);
+			}
+			cmdTrees.add(buildTreeForCommand(rootCmd, tokens));
+		}
+		return cmdTrees;
 	}
 	
 	private CommandNode buildTreeForCommand(String cmdName, Tokenizer tokens) {
@@ -56,20 +62,40 @@ public class Parser {
 		rootNode.setMyValue(cmdName);
 		rootNode.setOp(getOp(cmdName));
 		while (numArgs > 0) {
-			
+			String currentToken = tokens.nextToken();
+			if (Pattern.matches(CONSTANT, currentToken)) {
+				rootNode.addChild(makeLeafNode(currentToken));
+				numArgs--;
+			}
+			else if (Pattern.matches(VARIABLE, currentToken)) {
+				if (!isValidVar(currentToken)) {
+					throw new ParserException(ParserException.INVALID_VAR, currentToken);
+				}
+				rootNode.addChild(makeLeafNode(currentToken));
+				numArgs--;
+			}
+			else if (Pattern.matches(COMMAND, currentToken)) {
+				if (!operationFactory.isValidOp(currentToken)) {
+					throw new ParserException(ParserException.INVALID_CMD, currentToken);
+				}
+				rootNode.addChild(buildTreeForCommand(currentToken, tokens));
+				numArgs--;
+			}
+			else if (Pattern.matches(LISTSTART, currentToken)) {
+				
+			}
 		}
+		return rootNode;
+	}
+	
+	private CommandNode makeLeafNode(String value) {
+		CommandNode leafNode = new CommandNode();
+		leafNode.setMyValue(value);
+		return leafNode;
 	}
 	
 	private CommandOperation getOp(String cmdName) {
 		return operationFactory.getOp(cmdName);
-	}
-	
-	private String getRootCommand(String expression) {
-		String rootCmd = expression.split(" ")[0];
-		if (!operationFactory.isValidOp(rootCmd)) {
-			throw new ParserException(ParserException.INVALID_CMD, rootCmd);
-		}
-		return rootCmd;
 	}
 	
 	private String removeCommentsAndFlatten(String expression) {
@@ -93,6 +119,10 @@ public class Parser {
 	
 	private String regex(String syntax) {
 		return syntaxProperties.getProperty(syntax);
+	}
+	
+	private boolean isValidVar(String varName) {
+		return variableMap.containsKey(varName) && variableMap.get(varName) != null;
 	}
 	
 	private Properties getPropertiesFile(String filePath) {

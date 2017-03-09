28af21d49d6f04ec419fb49b1debc3696d99630f
jyang301 <jy183@duke.edu>
2017-03-09 00:43:03 -0500

    parser recursive method, tokenizer

--- src/controller/Parser.java
+++ src/controller/Parser.java
@@ -11,12 +11,27 @@ import java.util.Collection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
+import java.lang.StringBuilder;
 
 public class Parser {
 
-	private final String PROPERTIES_FILE = "resources/Arguments.properties";
+	private final String ARGS_PROPERTIES_FILE = "resources/Arguments.properties";
+	private final String SYNTAX_PROPERTIES_FILE = "resources.languages/Syntax.properties";
 	
-	private Properties properties;
+	private final String COMMENT = regex("Comment");
+	private final String CONSTANT = regex("Constant");
+	private final String VARIABLE = regex("Variable");
+	private final String COMMAND = regex("Command");
+	private final String LISTSTART = regex("ListStart");
+	private final String LISTEND = regex("ListEnd");
+	private final String GROUPSTART = regex("GroupStart");
+	private final String GROUPEND = regex("GroupEnd");
+	private final String WHITESPACE = regex("Whitespace");
+	private final String NEWLINE = regex("NewLine");
+	
+	private Properties argProperties;
+	private Properties syntaxProperties;
 	private Map<String, String> commandMap;
 	private Map<String, Double> variableMap;
 	private OperationFactory operationFactory;
@@ -25,18 +40,24 @@ public class Parser {
 		variableMap = varMap;
 		commandMap = cmdMap;
 		operationFactory = new OperationFactory(commandMap);
-		properties = getPropertiesFile();
+		argProperties = getPropertiesFile(ARGS_PROPERTIES_FILE);
+		syntaxProperties = getPropertiesFile(SYNTAX_PROPERTIES_FILE);
 	}
 	
 	private Collection<CommandNode> parseCommand(String expression) {
-		String rootCmd = getRootCommand(expression);
-		CommandNode rootNode = new CommandNode(getExpressionTree(rootCmd, expression));
-		rootNode.setMyValue(rootCmd);
+		expression = removeCommentsAndFlatten(expression);
+		Tokenizer tokens = new Tokenizer(expression, WHITESPACE);
+		
 	}
 	
-	private Collection<CommandNode> getExpressionTree(String rootCmd, String cmdBody) {
-		ArrayList<CommandNode> rootNodes = new ArrayList<CommandNode>();
-		
+	private CommandNode buildTreeForCommand(String cmdName, Tokenizer tokens) {
+		int numArgs = numArgs(cmdName);
+		CommandNode rootNode = new CommandNode();
+		rootNode.setMyValue(cmdName);
+		rootNode.setOp(getOp(cmdName));
+		while (numArgs > 0) {
+			
+		}
 	}
 	
 	private CommandOperation getOp(String cmdName) {
@@ -45,31 +66,42 @@ public class Parser {
 	
 	private String getRootCommand(String expression) {
 		String rootCmd = expression.split(" ")[0];
-		if (!isValidCommand(rootCmd)) {
+		if (!operationFactory.isValidOp(rootCmd)) {
 			throw new ParserException(ParserException.INVALID_CMD, rootCmd);
 		}
 		return rootCmd;
 	}
 	
-	private int numArgs(String cmdName) {
-		return Integer.parseInt(properties.getProperty(cmdName));
+	private String removeCommentsAndFlatten(String expression) {
+		String[] lines = expression.split(NEWLINE);
+		StringBuilder sb = new StringBuilder();
+		for (String line : lines) {
+			if (!Pattern.matches(COMMENT, line)) {
+				sb.append(line);
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
 	}
 	
-	private Properties getPropertiesFile() {
+	private int numArgs(String cmdName) {
+		if (operationFactory.isValidOp(cmdName)) {
+			return Integer.parseInt(argProperties.getProperty(cmdName));
+		}
+		else return 0;
+	}
+	
+	private String regex(String syntax) {
+		return syntaxProperties.getProperty(syntax);
+	}
+	
+	private Properties getPropertiesFile(String filePath) {
 		Properties prop = new Properties();
 		try {
-			prop.load(new FileInputStream(new File(PROPERTIES_FILE)));
+			prop.load(new FileInputStream(new File(filePath)));
 		} catch (Exception e) {
 			throw new ParserException(ParserException.PROPERTIES_ERROR);
 		}
 		return prop;
-	}
-	
-	private boolean isValidCommand(String cmd) {
-		return commandMap.containsKey(cmd) && commandMap.get(cmd).length() != 0;
-	}
-	
-	private boolean isValidVariable(String var) {
-		return variableMap.containsKey(var) && variableMap.get(var) != null;
 	}
 }

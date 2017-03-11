40c63744f9ed76ba1bf64758b62c1ec662b6effd
Yuxiang He <yuxiang.he@duke.edu>
2017-03-11 02:39:10 -0500

    more cleanup

--- src/Main.java
+++ src/Main.java
@@ -39,26 +39,6 @@ public class Main extends Application {
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.getKeyFrames().add(frame);
 		animation.play();
-
-		
-		//TODO hard coded testing code, to be removed
-//		ArrayList<FrontEndData> dataCollection = new ArrayList<>();
-//		FrontEndData data1 = new FrontEndData("test main 43", Language.Chinese);
-//		TurtleParameters params=new TurtleParameters(1, 125, 125, 45, true, true, true);
-//		data1.addTurtleParameters(params);
-//		dataCollection.add(data1);
-//		
-//		FrontEndData data2 = new FrontEndData("test main 51", Language.Chinese);
-//		TurtleParameters params2=new TurtleParameters(2, 125, 125, 45, true, true, true);
-//		data2.addTurtleParameters(params2);
-//		dataCollection.add(data2);
-//		display.show(dataCollection);
-		
-//		dataCollection = new ArrayList<>();
-//		data = new FrontEndData("test main 60", Language.Chinese);
-//		data.addError(new ErrorMessage("test error"));
-//		dataCollection.add(data);
-//		display.show(dataCollection);
 	}
 
 	private void step(double elapsedTime) {
@@ -68,12 +48,8 @@ public class Main extends Application {
 			try {
 				data = engine.runOp(rcmd);
 			} catch (Exception e) {
-
 				data = new FrontEndData(rcmd.getCommandString(), Language.English);
 				data.addError(new ErrorMessage(e));
-				
-				//TODO REMOVE THIS LINE!
-				e.printStackTrace();
 			}
 			ArrayList<FrontEndData> collec = new ArrayList<>();
 			collec.add(data);

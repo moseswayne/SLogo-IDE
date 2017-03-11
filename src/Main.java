5196a685ec0698b9e3de909555028599aaf0f2aa
Moses Wayne <moses.wayne@duke.edu>
2017-03-11 02:45:03 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import Model.ModelExecutionEngine;
+import Model.backEndUtils.SLogoSettings;
 import View.GUI;
 import View.viewUtils.FrontEndData;
 import javafx.animation.KeyFrame;
@@ -11,12 +12,13 @@ import javafx.util.Duration;
 import utils.ErrorMessage;
 import utils.Language;
 import utils.RawCommand;
-import utils.TurtleParameters;
 
 public class Main extends Application {
 	private final String TITLE = "SLogo Program";
+	private final String defaultSettings = "defaultSettings.properties";
 	GUI display;
 	ModelExecutionEngine engine;
+	SLogoSettings settings;
 
 	public static final int FRAMES_PER_SECOND = 60;
 	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
@@ -28,7 +30,8 @@ public class Main extends Application {
 		display.setNewWindowButton(() -> {
 			(new Main()).start(new Stage());
 		});
-		engine = new ModelExecutionEngine();
+		settings = new SLogoSettings(defaultSettings);
+		engine = new ModelExecutionEngine(settings);
 		stage.setTitle(TITLE);
 //		stage.setResizable(false);
 		Scene sc = display.getScene();
@@ -39,6 +42,26 @@ public class Main extends Application {
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.getKeyFrames().add(frame);
 		animation.play();
+
+		
+		//TODO hard coded testing code, to be removed
+//		ArrayList<FrontEndData> dataCollection = new ArrayList<>();
+//		FrontEndData data1 = new FrontEndData("test main 43", Language.Chinese);
+//		TurtleParameters params=new TurtleParameters(1, 125, 125, 45, true, true, true);
+//		data1.addTurtleParameters(params);
+//		dataCollection.add(data1);
+//		
+//		FrontEndData data2 = new FrontEndData("test main 51", Language.Chinese);
+//		TurtleParameters params2=new TurtleParameters(2, 125, 125, 45, true, true, true);
+//		data2.addTurtleParameters(params2);
+//		dataCollection.add(data2);
+//		display.show(dataCollection);
+		
+//		dataCollection = new ArrayList<>();
+//		data = new FrontEndData("test main 60", Language.Chinese);
+//		data.addError(new ErrorMessage("test error"));
+//		dataCollection.add(data);
+//		display.show(dataCollection);
 	}
 
 	private void step(double elapsedTime) {
@@ -48,8 +71,12 @@ public class Main extends Application {
 			try {
 				data = engine.runOp(rcmd);
 			} catch (Exception e) {
+
 				data = new FrontEndData(rcmd.getCommandString(), Language.English);
 				data.addError(new ErrorMessage(e));
+				
+				//TODO REMOVE THIS LINE!
+				e.printStackTrace();
 			}
 			ArrayList<FrontEndData> collec = new ArrayList<>();
 			collec.add(data);
